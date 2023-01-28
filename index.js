84aa395fd95b911cc0a830abf24f45feb083760f
vvkquau <ducnhan141106@gmail.com>
2023-01-28 13:11:04 +0700

    done

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-function phone() {
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText("0937840446");
-
-    /* Alert the copied text */
-    alert("You have copied my phone  number !");
+function func() {
+    navigator.clipboard.writeText("text");
+    alert("You have copied my  !");
 }
 
-function email() {
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText("ducnhan141106@gmail.com");
-
-    /* Alert the copied text */
-    alert("You have copied my email address !");
-}
-
-function bank() {
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText("1034561329");
-
-    /* Alert the copied text */
-    alert("You have copied my bank number account ✨");
-}
+// text
